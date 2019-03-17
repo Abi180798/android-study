3d86fcef7a837e3c4ada194f51b8b76f6d40d22f
Abi 180798 <habiburrahman180798@gmail.com>
2019-03-18 00:37:49 +0800

    login & intent

--- ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
+++ ProjectAndroid/app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -14,19 +14,39 @@ public class MainActivity extends AppCompatActivity {
     private EditText pass;
     private TextView info;
     private Button login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    private void verifyLogin(View view){
+
         user = findViewById(R.id.etUsername);
         pass = findViewById(R.id.etPassword);
-        info = findViewById(R.id.tvLogin);
         login = findViewById(R.id.btnlogin);
-        if (user == "admin" && pass == "pass"){
+        info = findViewById(R.id.tvLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyLogin(user.getText().toString(), pass.getText().toString());
+            }
+        });
+
+    }
+
+    private void verifyLogin(String username, String password){
+        if (username.equals("admin") && password.equals("pass")){
             Intent intent = new Intent(MainActivity.this, HalamanUtama.class);
+            startActivity(intent);
+        }else{
+            counter--;
+            info.setText("Batas Login Sisa: " + String.valueOf(counter));
+            if (counter == 0){
+                login.setEnabled(false);
+            }
+
         }
     }
+
 }
